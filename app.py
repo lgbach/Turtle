2c4867ea9bac13392768a0fd0dcd03c6b0735e5f
Gia Bach Le <85614994+lgbach@users.noreply.github.com>
2022-04-12 12:20:50 +1000

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-course = "Python for Beginners"
-print (course.replace('for', '4'))
-print (course)
-print ('Python' in course )
-print (10 * 3)
-word = input ('Type in a word: ')
-wordCAP = word.upper()
-print('UPPERCASE word: ' + wordCAP)
